04a0675723ce564d9bf011aa81653c40e6152c60
Sandeep Mishra <35232211+sandeepkumarmishra354@users.noreply.github.com>
2026-06-28 23:35:07 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site: "https://sandeepkumarmishra354.github.io",
-  base: "/shivay-construction"
+  // base: "/shivay-construction"
 });
